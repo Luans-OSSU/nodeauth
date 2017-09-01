6bbcb462e02fac5ca28e7b89b351efff65f01d7c
Luans <luansnain@gmail.com>
2017-09-01 19:06:23 +0200

    register form post, validation

--- app.js
+++ app.js
@@ -46,22 +46,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Validator
-app.use(expressValidator({
-  errorFormatter: function (param, msg, value) {
-    var namespace = param.split('.'),
-      root = namespace.shift(),
-      formParam = root;
-
-    while (namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param: formParam,
-      msg: msg,
-      value: value
-    };
-  }
-}));
+app.use(expressValidator());
 
 app.use(require('connect-flash')());
 app.use(function (req, res, next) {
